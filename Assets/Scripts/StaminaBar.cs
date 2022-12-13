2ad02fd255c367180e65ecac8fed9d8dbc31380f
Damien Ambrose <89913900+DamienAmbrose@users.noreply.github.com>
2022-12-13 17:26:35 +0530

    New Player class (#17)
    * Performance upgrades
    
    I've made a `Player` class which holds the movement functions. The current build is very botched, but it is still lightning fast compared to previous versions. The profiler is sticking close to 200 FPS rather than constant -60 FPS spikes.
    
    * Modulation
    
    I've moved the variables related to the player into their own class to more easily access them from other scripts.
    
    * Cleaning up
    
    I spent a day cleaning up the code and putting everything related to the player into its own script. Note that now, instead of writing `globalGameObject.GetComponent<Player>.[property name]`, you can just type `Player.PlayerObject.[property name]`. All scripts should now send data into the `Player` class via the `PlayerObject`. I've also fixed some bugs from my previous commit.
    
    * Minor bug fixes
    
    I made a few things easier to read and I fixed a couple of bugs. The migration is more or less done and is ready for review

--- Assets/Scripts/StaminaBar.cs
+++ Assets/Scripts/StaminaBar.cs
@@ -3,14 +3,10 @@ using UnityEngine.UI;
 
 public class StaminaBar : MonoBehaviour
 {
-    public GameObject playerGameobject;
-    private float stamina;
     public Slider sliderObject;
-
 
     private void Update()
     {
-        stamina = playerGameobject.GetComponent<PlayerMovement>().stamina;
-        sliderObject.value = stamina;
+        sliderObject.value = Player.PlayerObject.stamina;
     }
 }
