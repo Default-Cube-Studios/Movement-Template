742f9bf80054a14a6ecd74f1e2da1f73a92a4b80
Damien Ambrose <damienambrose@outlook.com>
2023-01-03 11:06:16 +0530

    Player manager system
    I've rewritten a lot of code to create a Player Manager ( #25 ) directly linked with the player itself

--- Assets/Scriptable Objects/PowerUpType.cs
+++ Assets/Scriptable Objects/PowerUpType.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "New Power Up", menuName = "Power Up")]
-public class PowerUp : ScriptableObject
+public class PowerUpType : ScriptableObject
 {
     #region Variable Initials
+    [Header("Colors")]
+    [ColorUsage(false)] public Color _color;
+    public float _colorIntensity;
+    [Header("Events")]
     [Tooltip("The event triggered when the power up is activated")] public UnityEvent OnCollect;
     [Tooltip("The event triggered when the power up timer finishes (Enable Is Timed first)")] public UnityEvent OnRevert;
     #endregion
