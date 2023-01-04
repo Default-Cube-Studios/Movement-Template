3798d8f9907380f7623a8b4653cdd0ab460c5738
adityamallkush <kushwaha.aditya2010@gmail.com>
2023-01-04 17:32:19 +0530

    Backup of all Scripts
    This is a backup commit of the scripts which were previously used.

--- Assets/Scripts/UI/FramesPerSecond.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using TMPro;
-
-[RequireComponent(typeof(TextMeshProUGUI))]
-public class FramesPerSecond : MonoBehaviour
-{
-    public void LateUpdate() => gameObject.GetComponent<TextMeshProUGUI>().text = (Mathf.Round(1 / Time.unscaledDeltaTime)).ToString();
-}
